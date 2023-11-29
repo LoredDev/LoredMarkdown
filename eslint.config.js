42f40910873cfaeacc61ba78dfb82bb79db3ba9e
Guz013 <contact.guz013@gmail.com>
2023-11-29 16:33:24 -0300

    feat(banners-lib): ✨ read svg files without node:fs
    Added to the SVG converting script a line to create .svg.js files.
    These files are normal JS files that exports just a single string,
    the SVG file content. This way the library doesn't need a build
    step to embed these files and/or node:fs to read them.

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,9 @@
 import { configs, defineConfig, presets } from '@eslegant/js';
 
 export default defineConfig([
+	{
+		ignores: ['**/*.svg.js'],
+	},
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	...presets.strict,
 	configs.environments.node.strict.default,
