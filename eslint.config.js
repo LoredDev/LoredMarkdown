08b66076f65641f46d0de92031cc254c4c1ccc90
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-01-16 18:58:00 -0300

    chore: 🔧 ignore test files

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import { configs, defineConfig, presets } from '@eslegant/js';
 
 export default defineConfig([
 	{
-		ignores: ['**/*.svg.js'],
+		ignores: ['**/*.svg.js', '**/*/test.*'],
 	},
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	...presets.strict,
