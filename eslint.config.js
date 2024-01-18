b0dfe09a1fe6abe860e9af059e231346b530285d
Gustavo "Guz" L. de Mello <contact.guz013@gmail.com>
2024-01-17 23:17:38 -0300

    refactor: ♻️ ignore more files

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,12 @@ import { configs, defineConfig, presets } from '@eslegant/js';
 
 export default defineConfig([
 	{
-		ignores: ['**/*.svg.js', '**/*/test.*'],
+		ignores: [
+			'**/*.svg.js',
+			'**/*/test.*',
+			'**/*.{woff,woff2,ttf}',
+			'**/*/package.json',
+		],
 	},
 	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
 	...presets.strict,
